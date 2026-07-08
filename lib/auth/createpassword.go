b6ceb1dba603cb5877e35a17714b2c8e35d65af1
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-08 08:06:45 +0800

    feat: change split code create password

---
+++ lib/auth/createpassword.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"authenticatiion-flow/lib/utils"
+	"fmt"
+)
+
+func CreatePassword() string {
+	var password1 string
+	var password2 string
+
+	fmt.Printf("Enter a strong password :  ")
+	fmt.Scanf("%s", &password1)
+
+	fmt.Printf("Confirm your password:  ")
+	fmt.Scanf("%s", &password2)
+
+	if password1 != password2 {
+		utils.Clear()
+		fmt.Printf("Wrong confirm password, press enter to back!  \n\n")
+		fmt.Scanf("\n")
+		CreatePassword()
+	}
+
+	return password1
+}
